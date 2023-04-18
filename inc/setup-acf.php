9070794215875a8ee561f74781b9b0d3f5aaa5ef
Anna Diaz-Tillman <annadiaztillman@Anna-MacBook-Pro.local>
2023-04-19 00:16:59 +0300

    ACF: add options page

--- inc/setup-acf.php
+++ inc/setup-acf.php
@@ -5,6 +5,35 @@
  * @package axio
  */
 
+
+/**
+ * Add ACF fields options page
+ */
+if (function_exists('acf_add_options_page')) {
+
+  // Main Theme Settings Page
+  $parent = acf_add_options_page( array(
+    'page_title'  => __('Ikaalinen Asetukset'),
+    'menu_title'  => __('Ikaalinen Asetukset'),
+    'menu_slug'   => 'theme-settings',
+    'capability'	=> 'edit_posts',
+    'redirect'    => 'Ikaalinen Asetukset',
+  ) );
+
+  //
+  // Global Options
+  // Same options on all languages.
+  //
+
+  acf_add_options_sub_page( array(
+    'page_title' => __('Yleinen'),
+    'menu_title' => __('Yleinen', 'text-domain'),
+    'menu_slug'  => 'general-options',
+    'parent'     => $parent['menu_slug']
+  ) );
+
+}
+
 /**
  * Save ACF fields as JSON at theme root /acf-json/
  */
